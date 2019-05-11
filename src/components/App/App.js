f30eee91063fb4e1406f1f7719f7b785521264b4
Nb123456789 <44652813+Nb123456789@users.noreply.github.com>
2019-05-11 23:53:30 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,29 +8,17 @@ class App extends React.Component {
 
   constructor() {
     super();
-    this.doSearch = this.doSearch.bind(this);
-    this.timeout=0;
     this.state = {
       tag: 'art'
     };
   }
-
-  doSearch(event){
-     var searchText = event.target.value;
-     if(this.timeout) clearTimeout(this.timeout);
-     this.timeout = setTimeout(function(){
-                      this.setState({
-                          tag: event.target.value
-                      })
-                    }.bind(this),500);
-   }
 
   render() {
     return (
       <div className="app-root">
         <div className="app-header">
           <h2>Flickr Gallery</h2>
-          <input className="app-input" onChange={event => this.doSearch({event})} value={this.state.tag}/>
+          <input className="app-input" onChange={event => this.setState({tag: event.target.value})} value={this.state.tag}/>
         </div>
         <Gallery tag={this.state.tag}/>
       </div>
