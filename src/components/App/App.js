92b600db198bd23d6f0d8de54f1cbde5baf7ad12
Nb123456789 <44652813+Nb123456789@users.noreply.github.com>
2019-05-12 21:21:49 +0300

    Task 2 - api calls

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,12 +5,26 @@ import Gallery from '../Gallery';
 class App extends React.Component {
   static propTypes = {
   };
+  static searchTimeout = 500
 
   constructor() {
     super();
+    this.timer = null
     this.state = {
       tag: 'art'
     };
+
+    this.searchChangeHandler = this.searchChangeHandler.bind(this);
+  }
+
+  searchChangeHandler(event) {
+    if (this.timer != null) {
+      clearTimeout(this.timer)
+    }
+    let tag = event.target.value
+    this.timer = setTimeout(()=> {
+      this.setState({tag: tag})
+    }, App.searchTimeout)
   }
 
   render() {
@@ -18,7 +32,7 @@ class App extends React.Component {
       <div className="app-root">
         <div className="app-header">
           <h2>Flickr Gallery</h2>
-          <input className="app-input" onChange={event => this.setState({tag: event.target.value})} value={this.state.tag}/>
+          <input className="app-input" onChange={this.searchChangeHandler} defaultValue={this.state.tag} />
         </div>
         <Gallery tag={this.state.tag}/>
       </div>
